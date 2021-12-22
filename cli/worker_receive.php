a362666993e5cbcb1391138381b5c896854054ff
Vitaliy <vitsgeen@yandex.ru>
2021-12-22 13:25:39 +0200

    fix

--- cli/worker_receive.php
+++ cli/worker_receive.php
@@ -10,7 +10,14 @@ require __DIR__ . '/../lib/common.php';
  * Arg 5. QUEUE_PASS
  * Arg 6. QUEUE_NAME
  */
-
+print '
+Arg 1. WORKER_NAME
+Arg 2. QUEUE_HOST
+Arg 3. QUEUE_PORT
+Arg 4. QUEUE_USER
+Arg 5. QUEUE_PASS
+Arg 6. QUEUE_NAME
+';
 
 commonConfig::checkStartedWorker($argv);
 commonConfig::processArgs($argv);
