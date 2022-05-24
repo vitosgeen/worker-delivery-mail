8c30f7e529a6f3cf554001e29fa18c8264ed8622
Vitaliy <vitosgeen@yandex.ru>
2022-05-24 13:14:53 +0300

    fix time wait

--- lib/common.php
+++ lib/common.php
@@ -28,7 +28,7 @@ class commonActs {
             };
             $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
             while ($channel->is_open()) {
-                $channel->wait();
+                $channel->wait(null, false, 180);
             }
             $channel->close();
             $queue_connect->close();
